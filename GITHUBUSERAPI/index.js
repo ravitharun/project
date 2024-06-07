c6ef078bcaf8c3bdbf4ee33a1a6cfe987c7f5350
Ravitharun <145847829+ravitharun@users.noreply.github.com>
2024-06-07 16:15:04 +0530

    Update index.js

--- GITHUBUSERAPI/index.js
+++ GITHUBUSERAPI/index.js
@@ -1,12 +1,27 @@
-function check() {
-    let user = prompt("enter your github name");
-    let url = `https://api.github.com/users/${user}`
-    fetch(url)
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-        })
 
+function check() {
+    let user = prompt("Enter your GitHub username");
+    console.log(user)
+    if (user == null) {
+        alert("FILL THE GITHUB NAME ")
+
+    }
+    else {
+        let url = `https://api.github.com/users/${user}`;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                let avatar_url = data.avatar_url;
+                let img = document.createElement('img');
+                img.src = avatar_url;
+                let container = document.getElementById('container');
+                container.appendChild(img);
+
+                let link = document.getElementById('click');
+                link.href = data.html_url;
+                link.innerHTML = link.href;
+            })
+            .catch(error => console.error('Error fetching data:', error));
+
+    }
 }
