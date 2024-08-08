feaf92e742a55469b1f257eecb2f0f2d02c2d636
Ravitharun <145847829+ravitharun@users.noreply.github.com>
2024-08-08 11:07:46 +0530

    Add files via upload

---
+++ New folder/index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var typed = new Typed('.typed', {
+      strings: ["a Fullstack Developer", "a UI/UX Designer", "an Enthusiastic Coder"],
+      typeSpeed: 50,
+      backSpeed: 50,
+      loop: true
+    });
+    console.log(typed)
+  });
